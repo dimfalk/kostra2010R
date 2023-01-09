00c9040052f5c75e34a0903e1befd5f2e694d889
falk-env <dimitri.falk1@gmail.com>
2023-01-09 21:52:25 +0100

    test(as_*): units

--- tests/testthat/test-get_returnp.R
+++ tests/testthat/test-get_returnp.R
@@ -6,6 +6,8 @@ test_that("Getting return periods for DWD-KOSTRA-2010R works.", {
 
   expect_equal(get_returnp(kostra_ref, hn = 72.3, d = 1440), units::as_units(c(30, 50), "a"))
 
+  expect_equal(get_returnp(kostra_ref, hn = 81.0, d = 1440), units::as_units(c(50, 100), "a"))
+
   expect_equal(get_returnp(kostra_ref, hn = 86.3, d = 1440), units::as_units(c(100, Inf), "a"))
 })
 
@@ -16,6 +18,8 @@ test_that("Getting interpolated return periods for DWD-KOSTRA-2010R works.", {
   expect_equal(get_returnp(kostra_ref, hn = 42.8, d = 1440, interpolate = TRUE), units::as_units(2, "a"))
 
   expect_equal(get_returnp(kostra_ref, hn = 72.3, d = 1440, interpolate = TRUE), units::as_units(39.9, "a"))
+
+  expect_equal(get_returnp(kostra_ref, hn = 81.0, d = 1440, interpolate = TRUE), units::as_units(94.4, "a"))
 
   expect_error(get_returnp(kostra_ref, hn = 86.3, d = 1440, interpolate = TRUE))
 })
@@ -38,6 +42,8 @@ test_that("Getting return periods for stats from DWA-A 531 works.", {
   expect_equal(get_returnp(stats_ref, hn = 40.7, d = 1440), units::as_units(c(2, 2), "a"))
 
   expect_equal(get_returnp(stats_ref, hn = 72.3, d = 1440), units::as_units(c(33, 50), "a"))
+
+  expect_equal(get_returnp(stats_ref, hn = 81.0, d = 1440), units::as_units(c(50, 100), "a"))
 
   expect_equal(get_returnp(stats_ref, hn = 86.3, d = 1440), units::as_units(c(100, Inf), "a"))
 })
