37d9a3e46b0efa101177f202ee4269945dcc76b2
Dimitri Falk <dimitri.falk1@gmail.com>
2024-02-28 13:43:58 +0100

    test(idx_*)

--- tests/testthat/test-idx_build.R
+++ tests/testthat/test-idx_build.R
@@ -1,6 +1,12 @@
 test_that("Building of 'INDEX_RC' out of X and Y information works.", {
 
+  expect_equal(idx_build(col = 0, row = 0), "0")
+
+  expect_equal(idx_build(col = 1, row = 2), "2001")
+
+  expect_equal(idx_build(col = 2, row = 1), "1002")
+
   expect_equal(idx_build(col = 11, row = 49), "49011")
 
-  expect_equal(idx_build(row = 10, col = 47), "10047")
+  expect_equal(idx_build(col = 78, row = 106), "106078")
 })
--- tests/testthat/test-idx_exists.R
+++ tests/testthat/test-idx_exists.R
@@ -1,9 +1,15 @@
-test_that("Sample grid cell is available in the dataset.", {
+test_that("Sample grid cells are available in the dataset.", {
+
+  expect_true(idx_exists("0"))
 
   expect_true(idx_exists("49011"))
+
+  expect_true(idx_exists("106078"))
 })
 
 test_that("Random grid cell is not available in the dataset.", {
 
-  expect_false(idx_exists("foobar"))
+  expect_false(idx_exists("79"))
+
+  expect_false(idx_exists("40477"))
 })
