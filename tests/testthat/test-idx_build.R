1b63b2924c68979e97b5869349074f7795ee7ee0
Dimitri Falk <dimitri.falk1@gmail.com>
2024-02-28 13:51:23 +0100

    test(idx_build)

--- tests/testthat/test-idx_build.R
+++ tests/testthat/test-idx_build.R
@@ -4,9 +4,9 @@ test_that("Building of 'INDEX_RC' out of X and Y information works.", {
 
   expect_equal(idx_build(col = 1, row = 2), "2001")
 
-  expect_equal(idx_build(col = 2, row = 1), "1002")
-
   expect_equal(idx_build(col = 11, row = 49), "49011")
+
+  expect_equal(idx_build(col = 5, row = 102), "102005")
 
   expect_equal(idx_build(col = 78, row = 106), "106078")
 })
