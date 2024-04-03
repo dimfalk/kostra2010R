f1cb711a77f06b9a50742d564c8a4d92a49f18f6
falk-env <dimitri.falk1@gmail.com>
2024-04-03 13:41:00 +0200

    doc(kostra_dwd_2010r): data types

--- R/data.R
+++ R/data.R
@@ -2,21 +2,21 @@
 #'
 #' Statistical precipitation values as a function of duration and return period
 #'
-#' @format A list of length 18 with simple feature collections of type POLYGON with 8,453 features and 10 fields each:
+#' @format A list of length 18 representing duration levels with simple feature collections of type POLYGON with 8,453 features and 11 fields each:
 #' \describe{
-#'   \item{INDEX_RC}{unique grid cell identifier}
+#'   \item{INDEX_RC}{numeric. Unique grid cell identifier.}
 #'
-#'   \item{HN_001A_}{design precipitation depth \code{[mm]} for return period tn = 1 a}
-#'   \item{HN_002A_}{design precipitation depth \code{[mm]} for return period tn = 2 a}
-#'   \item{HN_003A_}{design precipitation depth \code{[mm]} for return period tn = 3 a}
-#'   \item{HN_005A_}{design precipitation depth \code{[mm]} for return period tn = 5 a}
-#'   \item{HN_010A_}{design precipitation depth \code{[mm]} for return period tn = 10 a}
-#'   \item{HN_020A_}{design precipitation depth \code{[mm]} for return period tn = 20 a}
-#'   \item{HN_030A_}{design precipitation depth \code{[mm]} for return period tn = 30 a}
-#'   \item{HN_050A_}{design precipitation depth \code{[mm]} for return period tn = 50 a}
-#'   \item{HN_100A_}{design precipitation depth \code{[mm]} for return period tn = 100 a}
+#'   \item{HN_001A_}{numeric. Design precipitation depth \code{[mm]} for return period Tn = 1 a.}
+#'   \item{HN_002A_}{numeric. Design precipitation depth \code{[mm]} for return period Tn = 2 a.}
+#'   \item{HN_003A_}{numeric. Design precipitation depth \code{[mm]} for return period Tn = 3 a.}
+#'   \item{HN_005A_}{numeric. Design precipitation depth \code{[mm]} for return period Tn = 5 a.}
+#'   \item{HN_010A_}{numeric. Design precipitation depth \code{[mm]} for return period Tn = 10 a.}
+#'   \item{HN_020A_}{numeric. Design precipitation depth \code{[mm]} for return period Tn = 20 a.}
+#'   \item{HN_030A_}{numeric. Design precipitation depth \code{[mm]} for return period Tn = 30 a.}
+#'   \item{HN_050A_}{numeric. Design precipitation depth \code{[mm]} for return period Tn = 50 a.}
+#'   \item{HN_100A_}{numeric. Design precipitation depth \code{[mm]} for return period Tn = 100 a.}
 #'
-#'   \item{geometry}{polygon coordinates}
+#'   \item{geometry}{sfc_GEOMETRY. Coordinates.}
 #' }
 #' @source <https://opendata.dwd.de/climate_environment/CDC/grids_germany/return_periods/precipitation/KOSTRA/KOSTRA_DWD_2010R/gis/>
 #' @note Last access: 2022-10-20
